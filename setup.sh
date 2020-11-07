eb6b6c964fbc98736b102e2dbf721215ac1898ae
ajithbh <ajith_b_in@yahoo.com>
2020-11-07 23:52:29 +0800

    Version 2.0 - loop through the list.

--- setup.sh
+++ setup.sh
@@ -1,66 +1,108 @@
-#!/bin/bash - 
+#!/bin/bash -
 #===============================================================================
 #
 #          FILE: setup.sh
-# 
-#         USAGE: ./setup.sh 
-# 
-#   DESCRIPTION: 
-# 
+#
+#         USAGE: ./setup.sh
+#
+#   DESCRIPTION:
+#
 #       OPTIONS: None
-#  REQUIREMENTS: git, apt-get
-#          BUGS: 
+#  REQUIREMENTS: apt-get
+#          BUGS:
 #         NOTES: Scripts to setup new Ubuntu Linux installation
 #        AUTHOR: Ajith Bhaskaran
-#  ORGANIZATION: 
+#  ORGANIZATION:
 #       CREATED: 03/03/2014 16:01
-#      REVISION: 1.0
+#      REVISION: 2.0
 #===============================================================================
 
 set -o nounset                              # Treat unset variables as an error
 
-sudo apt-get -y install git-core subversion quilt tree curl
+NORM='\e[39m'
+RED='\e[91m'
+GREEN='\e[32m'
+YELLOW='\e[93m'
 
-sudo apt-get -y install vim-gtk meld minicom
+pkg_install()
+{
+    echo -e "${YELLOW}[Installing $@ ]${NORM}"
+    sudo apt-get -y install $@
+    [ $? = 0 ] && echo -e "${GREEN}[Done]${NORM}" || echo -e "${RED}[Fail]${NORM}"
+}
 
-sudo apt-get -y install smbfs cifs-utils nfs-kernel-server tftp-hpa tftpd-hpa
-
-sudo apt-get -y install squashfs-tools
-
-sudo apt-get -y install tofrodos
-sudo ln -s /usr/bin/fromdos /usr/bin/dos2unix
-sudo ln -s /usr/bin/todos /usr/bin/unix2dos
-
-sudo apt-get -y install zlibc zlib1g-dev
-
-sudo apt-get -y install libdirectfb-dev libtiff5-dev
-
-# Manpages
-sudo apt-get -y install manpages-posix manpages-posix-dev
-
-# UUID library
-sudo apt-get -y install uuid uuid-dev 
-
+PKGS=(
+# Develop
+build-essential
+libncurses-dev
+bison
+flex
+libssl-dev
+libelf-dev
+g++
+exuberant-ctags
+cscope
+meld
+indent
+cppcheck
+fakeroot
+python-pip
+virtualenv
+# Version control
+git-core
+subversion
+# patch management
+quilt
+# Editor
+vim-gtk
+# Network tools
+curl
+cifs-utils
+nfs-kernel-server
+tftp-hpa
+tftpd-hpa
+# Communication
+minicom
+# FS tools
+squashfs-tools
+# Utilities
+apt-file
+aptitude
+tree
+tofrodos
+zlibc
+zlib1g-dev
+uuid
+uuid-dev
+libdirectfb-dev
+libtiff5-dev
+scratchbox2
+iperf3
+tmux
+tofrodos
+# Shells
+zsh
+# VM
+qemu-kvm-extras
 # Data compression library
-sudo apt-get -y install liblzo2-2 liblzo2-dev
+liblzo2-2
+liblzo2-dev
+# Man pages
+manpages-posix
+)
 
-# Develop 
-sudo apt-get -y install build-essential libncurses-dev bison flex libssl-dev libelf-dev
+for pkg in ${PKGS[*]}; do
+    pkg_install $pkg
+done
 
-sudo apt-get -y install g++ exuberant-ctags cscope indent cppcheck fakeroot
+# Setup links for tofrodos
+[ ! -e /usr/bin/fromdos ] && sudo ln -s /usr/bin/fromdos /usr/bin/dos2unix
+[ ! -e /usr/bin/todos ] && sudo ln -s /usr/bin/todos /usr/bin/unix2dos
 
-sudo apt-get -y install python-pip
-
-sudo pip install virtualenv
-
-# Scratchbox2
-sudo apt-get -y install scratchbox2 qemu-kvm-extras
-
-sudo apt-get -y install apt-file
+# Update apt-file database
 sudo apt-file update
 
-sudo apt-get -y install tmux
-
+# Configure tmux
 cat > ~/.tmux.conf << _EOF
 set -g prefix C-a
 unbind-key C-b
@@ -86,7 +128,7 @@ _EOF
 
 
 # qemu_version=`qemu-arm --version | awk ' /version/ {print $3}'`
-# Scratchbox2 needs atleast version 0.13 of qemu 
+# Scratchbox2 needs atleast version 0.13 of qemu
 # cd /tmp/
 # wget http://wiki.qemu-project.org/download/qemu-0.13.0.tar.gz
 # tar xvzf qemu-0.13.0.tar.gz
@@ -95,13 +137,15 @@ _EOF
 # make && sudo make install
 # cd ~
 
-# cd /tmp/
-# wget http://stats.es.net/software/iperf-3.0.1.tar.gz
-# tar xvzf iperf-3.0.1.tar.gz 
-# cd iperf-3.0.1/
-# ./configure
-# make && sudo make install
-# cd ~
+if [ ! -e /usr/bin/iperf3 ]; then
+    cd /tmp/
+    wget http://stats.es.net/software/iperf-3.0.1.tar.gz
+    tar xvzf iperf-3.0.1.tar.gz
+    cd iperf-3.0.1/
+    ./configure
+    make && sudo make install
+    cd ~
+fi
 
 if [ ! -e /usr/bin/quilt ]; then
     cd ~/Downloads
@@ -114,12 +158,10 @@ if [ ! -e /usr/bin/quilt ]; then
     cd ~
 fi
 
-
 ./setup_vim.sh
 
 echo "-i4 -nbad -bap -bbb -cdb -sc -br -ce -cdw -cli4 -cbi0 -ss -npcs -cs -bs -saf -sai -saw -nbc -npsl -brs -blf -lp -ip0 -ppi1 -il0 -bbo -nprs -nut -sob -nfca -d0 -di1 -l200" > ~/.indent.pro
 
-sudo apt-get -y install zsh
-
+# Set default shell as zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
