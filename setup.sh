c5a5e17596e334a7e6b807368211437d5ccf4e31
ajithbh <ajith_b_in@yahoo.com>
2021-03-15 05:56:46 +0000

    refactor: Separate into functions
    Separate into different functions so as to allow optional calls to
    setup specific items.

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@
 #        AUTHOR: Ajith Bhaskaran
 #  ORGANIZATION:
 #       CREATED: 03/03/2014 16:01
-#      REVISION: 2.0
+#      REVISION: 2.1
 #===============================================================================
 
 set -o nounset                              # Treat unset variables as an error
@@ -91,21 +91,22 @@ liblzo2-dev
 manpages-posix
 )
 
-for pkg in ${PKGS[*]}; do
-    pkg_install $pkg
-done
+setup_install_packages()
+{
+    for pkg in ${PKGS[*]}; do
+        pkg_install $pkg
+    done
 
-# Setup links for tofrodos
-[ ! -e /usr/bin/fromdos ] && sudo ln -s /usr/bin/fromdos /usr/bin/dos2unix
-[ ! -e /usr/bin/todos ] && sudo ln -s /usr/bin/todos /usr/bin/unix2dos
+    # Update apt-file database
+    sudo apt-file update
+}
 
-# Update apt-file database
-sudo apt-file update
-
-# Download tmux plugin manager
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-# Configure tmux
-cat > ~/.tmux.conf << _EOF
+setup_tmux()
+{
+    # Download tmux plugin manager
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    # Configure tmux
+    cat > ~/.tmux.conf << _EOF
 set -g prefix C-a
 unbind-key C-b
 bind-key C-a send-prefix
@@ -127,43 +128,74 @@ set -g @plugin 'tmux-plugins/tmux-resurrect'
 # Initialize TMUX plugin manager (keep this line at the very bottom of tmux.conf)
 run '~/.tmux/plugins/tpm/tpm'
 _EOF
+}
 
+setup_manual_check_install()
+{
+    # qemu_version=`qemu-arm --version | awk ' /version/ {print $3}'`
+    # Scratchbox2 needs atleast version 0.13 of qemu
+    # cd /tmp/
+    # wget http://wiki.qemu-project.org/download/qemu-0.13.0.tar.gz
+    # tar xvzf qemu-0.13.0.tar.gz
+    # cd qemu-0.13.0/
+    # ./configure
+    # make && sudo make install
+    # cd ~
 
-# qemu_version=`qemu-arm --version | awk ' /version/ {print $3}'`
-# Scratchbox2 needs atleast version 0.13 of qemu
-# cd /tmp/
-# wget http://wiki.qemu-project.org/download/qemu-0.13.0.tar.gz
-# tar xvzf qemu-0.13.0.tar.gz
-# cd qemu-0.13.0/
-# ./configure
-# make && sudo make install
-# cd ~
+    if [ ! -e /usr/bin/iperf3 ]; then
+        cd /tmp/
+        wget http://stats.es.net/software/iperf-3.0.1.tar.gz
+        tar xvzf iperf-3.0.1.tar.gz
+        cd iperf-3.0.1/
+        ./configure
+        make && sudo make install
+        cd ~
+    fi
 
-if [ ! -e /usr/bin/iperf3 ]; then
-    cd /tmp/
-    wget http://stats.es.net/software/iperf-3.0.1.tar.gz
-    tar xvzf iperf-3.0.1.tar.gz
-    cd iperf-3.0.1/
-    ./configure
-    make && sudo make install
-    cd ~
-fi
+    if [ ! -e /usr/bin/quilt ]; then
+        cd ~/Downloads
+        wget http://download.savannah.gnu.org/releases/quilt/quilt-0.60.tar.gz
+        tar xvzf quilt-0.60.tar.gz
+        cd quilt-0.60
+        ./configure
+        make
+        sudo make install
+        cd ~
+    fi
+}
 
-if [ ! -e /usr/bin/quilt ]; then
-    cd ~/Downloads
-    wget http://download.savannah.gnu.org/releases/quilt/quilt-0.60.tar.gz
-    tar xvzf quilt-0.60.tar.gz
-    cd quilt-0.60
-    ./configure
-    make
-    sudo make install
-    cd ~
-fi
+setup_vim()
+{
+    ./setup_vim.sh
+}
 
-./setup_vim.sh
+setup_indent()
+{
+    echo "-i4 -nbad -bap -bbb -cdb -sc -br -ce -cdw -cli4 -cbi0 -ss -npcs -cs -bs -saf -sai -saw -nbc -npsl -brs -blf -lp -ip0 -ppi1 -il0 -bbo -nprs -nut -sob -nfca -d0 -di1 -l200" > ~/.indent.pro
+}
 
-echo "-i4 -nbad -bap -bbb -cdb -sc -br -ce -cdw -cli4 -cbi0 -ss -npcs -cs -bs -saf -sai -saw -nbc -npsl -brs -blf -lp -ip0 -ppi1 -il0 -bbo -nprs -nut -sob -nfca -d0 -di1 -l200" > ~/.indent.pro
+setup_zsh()
+{
+    # Set default shell as zsh
+    sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+}
 
-# Set default shell as zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+setup_all()
+{
+    setup_install_packages
+    setup_manual_check_install
+    setup_tmux
+    setup_vim
+    setup_indent
+    setup_zsh
+}
 
+usage()
+{
+    echo "usage: "
+    echo "  $0 [option]"
+    echo "    option:  all | install_packages | tmux | vim | indent | zsh | manual_check_install"
+}
+
+[ $# -ge 1 ] && setup_"$@" || { usage; exit 1; }
+[ $? == 1 ] && { echo "Error: check log for details"; exit 1; }
